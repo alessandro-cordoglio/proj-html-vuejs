2eec0574bc677b629463dbb61c26788550e43355
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-12-05 23:48:02 +0100

    add: stile alla sezione hero

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,19 @@
 <script>
     export default {
+        data(){
+            return{
+                links:[
+                    'HOME',
+                    'ABOUT',
+                    'SERVICES',
+                    'PRICING',
+                    'BLOG',
+                ]
+            }
+        },
         components:{
-        }
+        },
+        
     }
 </script>
 
@@ -28,11 +40,31 @@
             </div>
         </section>
         <section class="hero_section">
-            <nav class="container_main">
-                <div class="nav_logo">
-                    <img src="../assets/logo.svg" alt="">
+            <div class="hero text-white">
+                <nav class="container_main nav_layout d-flex align-items-center">
+                    <div class="nav_logo">
+                        <img src="../assets/logo.svg" alt="">
+                    </div>
+                    <div class="nav_links">
+                        <ul>
+                            <li v-for="link in links">
+                                <a href="">{{link}}</a>
+                            </li>
+                            <button  class="my_button-fill"> GET IN TOUCH </button>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="hero_content container_hero">
+                    <div class="text-center">
+                        <h1 class="title">Lead ~ Customer</h1>
+                        <span>Succesful strategies require data analysis, creativity and customer focus, taking advantage of the lastest technologies to offer excellent services.</span>
+                        <div class="buttons mt-5">
+                            <button  class="text-white my_button-fill me-3"> GET IN TOUCH </button>
+                            <button  class="text-white my_button"> READ MORE</button>
+                        </div>
+                    </div>
                 </div>
-            </nav>
+            </div>
         </section>
     </header>
 </template>
@@ -42,14 +74,41 @@
         background-color: #21333E;
         padding: 10px;
     }
-    .hero_section{
-        background-image: url(../assets/hero-image.png);
-        filter: brightness(0.4);
-        background-size: cover;
-
-        .nav_logo{
-            width: 200px;
+    .hero{
+        background-color: black;
+        .nav_layout{
+            padding-top: 20px;
+            justify-content: space-between;
+            
+            .nav_logo img{
+                width: 200px ;
+            }
+            .nav_links{
+                font-size: 18px;
+                li{
+                    margin-left: 20px;
+                }
+                .my_button-fill{
+                    margin-left: 30px;
+                    color: white;
+                }
+            }
         }
+        .hero_content{
+            margin-top: 200px;
+            padding: 50px 0;
+            h1{
+                font-size: 80px;
+                font-weight: 900;
+            }
+            .buttons{
+                button{
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+                }
+            }
+        }
+
     }
 
 </style>
