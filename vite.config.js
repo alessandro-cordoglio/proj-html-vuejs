92de8af796ea094c4f039851c90a01b1e99057bd
Alessandro Cordoglio <alessandro.cord20@gmail.com>
2024-01-19 17:05:12 +0100

    hju

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<proj-html-vuejs>/',
   plugins: [vue()],
   base: "/proj-html-vuejs/",
   css: {
